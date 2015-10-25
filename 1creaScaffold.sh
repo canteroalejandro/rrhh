5b5dc32d6d99747c7e6255c73540b8954059b851
valeriianaa <valeriianaa@gmail.com>
2015-10-24 22:36:40 -0300

    .

--- 1creaScaffold.sh
+++ 1creaScaffold.sh
@@ -6,11 +6,11 @@ rails g scaffold Departamento codigo:string nombre:string fechaEnQueDirige:datet
 
 rails g scaffold Proyecto codigo:string nombre:string
 
-rails g scaffold Horario codigo:string nombre:string diaInicio:integer diaFin:integer horaInicio:time horaFin:time
+rails g scaffold Horario codigo:string nombre:string horaEntrada:time horaSalida:time margenEntradaCheck:boolean margenSalidaCheck:boolean inicioMargenEntrada:time finMargenEntrada:time inicioMargenSalida:time finMargenSalida:time
 
 rails g scaffold Usuario userName:string password:string rol:string
 
-rails g scaffold Asistencia entrada:boolean salida:boolean outputAsistencia:string horasTrabajadas:integer
+rails g scaffold Asistencia entrada:boolean salida:boolean outputAsistencia:string horasTrabajadas:decimal
 
 rails g scaffold HistoriaMedica codigo:string fecha:date doctor:string
 
